a2d1a8492c5e2f476fa7ab1586de3ae7359c9fa7
Atharva <atharvasharma.cs25@gmail.com>
2025-01-10 16:07:53 +0530

    Initial commit

--- level1_pe/FindAge.java
+++
@@ -1,6 +0,0 @@
-public class FindAge{
-public static void main(String[]args){
-   int birthYear = 2000;
-   int currentYear = 2024;
-   System.out.println("Harry's Current Age is :" + (currentYear - birthYear));
-   }}
