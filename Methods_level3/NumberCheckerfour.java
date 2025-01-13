ec142f477d8b99b00ebd5f4f748a6735f19b2b13
Atharva <atharvasharma.cs25@gmail.com>
2025-01-13 15:23:39 +0530

    Number Checker 4 added

---
+++ Methods_level3/NumberCheckerfour.java
@@ -0,0 +1,48 @@
+//making a class for required number check 
+public class NumberCheckerfour {
+    public static void main(String[] args) {
+        int number = 153; // Example number
+
+        System.out.println("Is Prime: " + isPrime(number));
+        System.out.println("Is Neon: " + isNeon(number));
+        System.out.println("Is Spy: " + isSpy(number));
+        System.out.println("Is Automorphic: " + isAutomorphic(number));
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) return false;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static boolean isNeon(int number) {
+        int square = number * number;
+        return sumDigits(square) == number;
+    }
+
+    public static boolean isSpy(int number) {
+        int sum = sumDigits(number);
+        int product = 1;
+        while (number != 0) {
+            product *= number % 10;
+            number /= 10;
+        }
+        return sum == product;
+    }
+
+    public static boolean isAutomorphic(int number) {
+        int square = number * number;
+        return String.valueOf(square).endsWith(String.valueOf(number));
+    }
+
+    public static int sumDigits(int number) {
+        int sum = 0;
+        while (number != 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+}
